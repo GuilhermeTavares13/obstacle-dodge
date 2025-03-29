e5265478f01e42ae75b29dd98d7ac95f2ceb3c95
Guilherme Tavares <gui.tavares2002.contato@gmail.com>
2025-03-29 13:40:25 -0300

    Update files

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,16 +1,24 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Mover : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 10f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        transform.Translate(1,2,-3);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float xValue = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        float yValue = 0f; // SerializeField significa que posso alterar os valores do objeto na Unity
+        float zValue = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        transform.Translate(xValue, yValue, zValue);
     }
 }
